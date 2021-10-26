fcb3e43524a9ad5056bc7d0ec69e0cdf79d4cbe3
dangkhieutrungthanh <khieuthanhtb19@gmail.com>
2021-10-26 07:21:58 +0700

    them doi tuong person

---
+++ Migrations/202110260019133_Creat_Table_Person.cs
@@ -0,0 +1,16 @@
+namespace DangKhieuTrungThanh.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Creat_Table_Person : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Models/person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DangKhieuTrungThanh.Models
+{
+    [Table("person")]
+    public class person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string PersonName { get; set; }
+    }
+}
